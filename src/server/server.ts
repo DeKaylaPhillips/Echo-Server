8bba18f34f0579c60a0150279509d679bf9ef091
DeKayla Phillips <dphillips@8thlight.com>
2023-07-21 12:33:27 -0400

    Add readable stream and method to close client connection
    Added 'data' event and callback function to log the message sent from a client to the server, and method to close the connection when the client has written to the server. Refactored log statements and added write() method stream data prompt to format the display of the TCP data sent from the server to the client and vice versa.

--- src/server/server.ts
+++ src/server/server.ts
@@ -16,10 +16,17 @@ class Server {
   openTCPConnection (): void {
     this.server = net.createServer((socket) => {
       this.socket = socket
-      this.socket.write('Connection established.')
+      this.socket.write(
+        'Connection established.\n\nPlease enter a message:\n\n'
+      )
+      this.socket.on('data', (data: Buffer) => {
+        const message = data.toString().trim()
+        console.log(`Client says...:\n\n${message}`)
+        this.socket.end('Client connection closed.\n')
+      })
     })
     this.server.on('connection', () => {
-      console.log('Client has connected.')
+      console.log('Client has connected.\n')
     })
     this.server.listen(this.port, this.host, (): void => {
       console.log(this.connectionMsg)
